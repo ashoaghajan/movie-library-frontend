b3dd8bf54171e148704283098ad4295524f0622a
ashoaghajan <ashoaghajan@gmail.com>
2026-04-29 13:59:22 +0400

    fix: notifications fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
+import { Notifications } from '@mantine/notifications';
+import '@mantine/notifications/styles.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -11,9 +13,8 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <MantineProvider
-        defaultColorScheme="dark"
-      >
+      <MantineProvider defaultColorScheme="dark">
+        <Notifications position="bottom-right" />
         <App />
       </MantineProvider>
     </QueryClientProvider>
